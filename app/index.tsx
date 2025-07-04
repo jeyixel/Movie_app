195f9686c517ab56ae2245b18e68156c8209f87c
jeyixel <jjayawardene610@gmail.com>
2025-07-04 15:00:06 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-5xl text-blue-500 font-bold">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
