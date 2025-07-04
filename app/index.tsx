383b513bacbf186d2b4ab10b1b2587b7d75eb3f1
jeyixel <jjayawardene610@gmail.com>
2025-07-04 16:17:05 +0530

    added navigation from welcome screen to onboard

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-5xl text-blue-500 font-bold">
-        Edit app/index.tsx to edit this screen.
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">
+        Welcome!
       </Text>
+      <Link href="/onboard">
+        Onboarding
+      </Link>
     </View>
   );
 }
