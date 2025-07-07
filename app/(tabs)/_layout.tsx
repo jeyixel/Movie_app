b8c365ab1404886b5341dae87dfa0f74a72adf83
jeyixel <jjayawardene610@gmail.com>
2025-07-07 12:41:53 +0530

    Added styling to the navigation bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,72 @@
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
+import { Image, ImageBackground, Text, View } from 'react-native'
+
+const TabIcon = ({focused, icon, title}: any) => {
+
+    if(focused){
+        return(
+            <ImageBackground
+                source={images.highlight}
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'>
+
+                <Image source={icon} tintColor="#151312" className="size-5"/>
+                <Text className='text-secondary text-base font-semibold'>{title}</Text>
+            </ImageBackground>
+        )
+    }
+
+    return(
+        <View className='size-full justify-center items-center mt-4 rounded-full'>
+            <Image source={icon} tintColor="#A8B5DB" className='size-5'/>
+        </View>
+    )
+
+    
+}
 
 const _layout = () => {
   return (
 
     // these are used to hide the top
 
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50, 
+                marginBottom: 36,
+                marginHorizontal: 20,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0f0D23',
+            }
+        }}
+    >
         <Tabs.Screen
             name = "index"
             options={{
                 title: 'Home', // home cuz this is the home page
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.home} 
+                        title="Home"
+                    />
+                )
             }}
         />
 
@@ -19,7 +74,14 @@ const _layout = () => {
             name = "search"
             options={{
                 title: 'Search',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.search} 
+                        title="Search"
+                    />
+                )
             }}
         />
 
@@ -27,7 +89,14 @@ const _layout = () => {
             name = "saved"
             options={{
                 title: 'Saved',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.save} 
+                        title="Saved"
+                    />
+                )
             }}
         />
 
@@ -35,7 +104,14 @@ const _layout = () => {
             name = "profile"
             options={{
                 title: 'Profile',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.person} 
+                        title="Profile"
+                    />
+                )
             }}
         />
 
