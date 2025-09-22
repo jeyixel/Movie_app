9d650c86b73f44a76bf3b5c27e0814bf1fc9d7ca
jeyixel <jjayawardene610@gmail.com>
2025-09-22 15:22:41 +0530

    1:52:16 wenkn wge kra itin

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,14 +4,17 @@ import { images } from '@/constants/images';
 import { fetchFromTMDB } from '@/services/api';
 import useFetch from '@/services/useFetch';
 import { useRouter } from 'expo-router';
-import React, { useEffect } from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-screens';
 
 const search = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
   const router = useRouter();
 
   // destructured properties from useFetch hook (autoFetch = false)
-  const { data: movies, loading: moviesLoading, error: moviesError, refetch } = useFetch(() => fetchFromTMDB({ query: '' }), false);
+  const { data: movies, loading, error, refetch } = useFetch(() => fetchFromTMDB({ query: '' }), false);
 
   useEffect(() => {
     refetch();
@@ -20,7 +23,7 @@ const search = () => {
   return (
     // background image and color
     <View className='flex-1 bg-primary'>
-      <Image source={images.bg} className='flex-1 absolute w-full z-0 resizeMode = "cover"' />
+  <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
 
       {/* rendering all movies */}
       <FlatList 
@@ -42,8 +45,32 @@ const search = () => {
           <>
             <View className='w-full flex-row justify-center mt-20 items-center'>
               <Image source={icons.logo} className='w-12 h-10' />
-              <Text className='text-white text-lg font-bold ml-2'>Search</Text>
             </View>
+
+            <View className='my-5'>
+              <SearchBar
+                placeholder='Search for a movie'/>
+            </View>
+
+            {/* loading screen */}
+            {loading && (
+              <ActivityIndicator size='large' color='#0000ff' className='my-3'/>
+            )}
+
+            {/* if there's an error */}
+            {error && (
+              <Text className = 'text-red-500 px-5 my-3'>Error: {error.message}</Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">SEARCH TERM</Text>
+                </Text>
+              )}
           </>
         }
       />
